c60b2b67b545fb3afb2ee068315a56fea0d18f1c
darrix <dmhendricks@gmail.com>
2019-11-17 08:44:59 -0500

    Added second player
    Fixed camera position in P1 for now

---
+++ Assets/RichSpin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RichSpin : MonoBehaviour
+{
+    public enum SpinAxis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    public float degreesPerFrame = 5.0f;
+
+    public SpinAxis currentAxis = SpinAxis.X;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAxis == SpinAxis.X)
+        {
+            transform.Rotate(degreesPerFrame,0,0);
+        }
+        else if (currentAxis == SpinAxis.Y)
+        {
+            transform.Rotate(0, degreesPerFrame, 0);
+        }
+        else
+        {
+            transform.Rotate(0,0,degreesPerFrame);
+        }
+    }
+}
